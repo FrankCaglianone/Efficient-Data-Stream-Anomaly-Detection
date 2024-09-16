3a9feac8efbd755a16f03ea08f7cda0d659b8c34
Francesco Caglianone <francescocaglianone@gmail.com>
2024-09-16 21:09:29 +0200

    dummy

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ parallel_anomaly_detection(data_stream)
 
 
 
-#* NoteSliding Window: Instead of clearing the entire buffer after each detection round, we now implement a sliding window approach by keeping most of the buffer (removing only step_size points) to preserve the recent history of the data stream.
+#* Note Sliding Window: Instead of clearing the entire buffer after each detection round, we now implement a sliding window approach by keeping most of the buffer (removing only step_size points) to preserve the recent history of the data stream.
 
 
 
